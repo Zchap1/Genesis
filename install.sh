4375ecbb549a574530a502852b54649ff8a40570
Zchap <zack@chapfam.net>
2018-02-17 18:00:36 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp genesis /bin/genesis;
-cp genesis /usr/bin/genesis;
-cp * -r /opt/genesis/;
+cp Genesis /bin/genesis;
+cp Genesis /usr/bin/genesis;
+cp * -r /opt/;
 echo "install complete! run genesis!"
 
